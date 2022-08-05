88fa78cd41463358091d22390c5e22edebe302f5
talles-alexandre <alexandretalles13@gmail.com>
2022-08-04 23:26:06 -0300

    commit_2

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import ReactDOM from "react-dom";
+import "./assets/css/reset.css";
+import "./assets/css/style.css";
+import App from "./componenetes/App";
+
+ReactDOM.render(<App />, document.querySelector(".root"));
